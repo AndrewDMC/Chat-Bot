20359a1a3f80b1369520d5e9f00587b30032e3bb
Andrea de Amicis <52075779+AndrewDMC@users.noreply.github.com>
2023-03-24 15:34:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 messages = []
-messages.append({"role": "system", "content": "you are a educator bot for school"})
+messages.append({"role": "system", "content": "Sei un robot utilizzato per dare informazioni inerenti alla scuola a ragazzini di 10 anni. Sii simpatico e fai battute."})
 
 print("Your new assistant is ready!")
 while input != "quit()":
@@ -11,7 +11,8 @@ while input != "quit()":
     messages.append({"role": "user", "content": message})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=messages)
+        messages=messages,
+        max_tokens=100)
     reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": reply})
     print("\n" + reply + "\n")
