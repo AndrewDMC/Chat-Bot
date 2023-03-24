5b33cf7b4d520eb5d4abb616b69fd860abeeba22
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2023-03-24 16:57:17 +0100

    🖊 Server and Client finished.

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9000
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    s.send("Hello".encode("utf-8"))
+    
+    while True:
+        data = s.recv(1024)
+        print("Received: " + data.decode("utf-8"))
+
+        message = input("Message: ")
+        s.send(message.encode("utf-8"))
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import openai
 import os
 import socket
 
-HOST = "0.0.0.0"
-PORT = 8080
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9000
 
 def main():
+    load_dotenv()
+
+    API_KEY = os.environ.get("OPENAI_API_KEY")
+    openai.api_key = API_KEY
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen()
     print(f"Listening on {HOST}:{PORT}...")
 
     conn, addr = s.accept()
+    message = conn.recv(1024)
+    print("Received " + message.decode("utf-8"))
+    conn.send("Welcome to NAO Chat!".encode("utf-8"))
 
     messages = []
     messages.append(
@@ -42,6 +50,13 @@ def main():
 
         while True:
             data = conn.recv(1024)
+
+            messages.append(
+                {
+                    "role": "user",
+                    "content": data.decode("utf-8")
+                }
+            )
             
             if not data:
                 break
@@ -51,25 +66,8 @@ def main():
                 messages = messages
             )
 
-
-    print("Your new assistant is ready!")
-
-    API_KEY = os.environ.get('OPENAI_API_KEY')
-
-    print(f"NAO Chat.\nUsing this API Key: {API_KEY}.\n")
-
-
-    while input != "quit()":
-        message = input(str("Your message: "))
-        messages.append({"role": "user", "content": message})
-
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages)
-
-        reply = response["choices"][0]["message"]["content"]
-        messages.append({"role": "assistant", "content": reply})
-        print("\n" + reply + "\n")
-
+            openai_response = response["choices"][0]["message"]["content"]
+            print(f"\n{openai_response}\n")
+            conn.send(openai_response.encode("utf-8"))
 if __name__ == "__main__":
     main()
