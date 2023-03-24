74bfee6a9ca26b9307d0ade07cfcb7c58650fc73
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2023-03-24 15:38:14 +0100

    ⛔ Removed files included in .gitignore
    @AndrewDMC coglione non mettere i .env su git a prescindere dal fatto che la repo sia privata o meno che dopo Copilot te lo frega.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from dotenv import load_dotenv
 import openai
+import os
 
-openai.api_key = "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"
+load_dotenv()
+
+API_KEY = os.environ.get('OPENAI_API_KEY')
+openai.api_key = f"{API_KEY}"
+
+print(f"{openai.api_key}")
 
 messages = []
 messages.append({"role": "system", "content": "you are a educator bot for school"})
 
 print("Your new assistant is ready!")
 while input != "quit()":
-    message = input()
+    message = input(str("Your message: "))
     messages.append({"role": "user", "content": message})
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
