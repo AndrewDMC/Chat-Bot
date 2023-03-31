0ee88ffabe0b1541fa1b0051056c35497d310f8f
Cristiano Granchelli <84400048+DevPisgeom@users.noreply.github.com>
2023-03-31 16:45:56 +0200

    miglioramento del riconoscimento vocale

--- stt.py
+++ stt.py
@@ -1,27 +1,25 @@
 import speech_recognition as sr
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 FILENAME="output.wav"
 
 def stt():
 	# Initialize recognizer
 	r = sr.Recognizer()
-
+	r.pause_threshold = 1
 	# Record audio
 	with sr.Microphone() as source:
 		print("Say something!")
-		audio = r.listen(source)
+		r.adjust_for_ambient_noise(source, 0.5)
+		audio = r.listen(source, phrase_time_limit=5, snowboy_configuration=None)
 	# Save audio to file
 	with open(FILENAME, "wb") as f:
 		f.write(audio.get_wav_data())
-	with sr.AudioFile(FILENAME) as source:
-		audio = r.record(source)
   
 	audio_file = open(FILENAME, "rb")
 	transcript = openai.Audio.transcribe("whisper-1", audio_file)
 	return transcript["text"]
 	
-
 print(stt())
