cc93730c420c8e33630a3e11f068e42eb19eefdc
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2023-04-05 14:08:38 +0200

    ✨ Formatting.

--- main.py
+++ main.py
@@ -7,24 +7,27 @@ import speech_recognition as sr
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9000
 VOICE_PORT = 9001
-FILENAME="output.wav"
+FILENAME = "output.wav"
+
 
 def stt():
-	# Initialize recognizer
-	r = sr.Recognizer()
-	r.pause_threshold = 1
-	# Record audio
-	with sr.Microphone() as source:
-		print("Say something!")
-		r.adjust_for_ambient_noise(source, 0.5)
-		audio = r.listen(source, phrase_time_limit=5, snowboy_configuration=None)
-	# Save audio to file
-	with open(FILENAME, "wb") as f:
-		f.write(audio.get_wav_data())
-  
-	audio_file = open(FILENAME, "rb")
-	transcript = openai.Audio.transcribe("whisper-1", audio_file)
-	return transcript["text"]
+    # Initialize recognizer
+    r = sr.Recognizer()
+    r.pause_threshold = 1
+    # Record audio
+    with sr.Microphone() as source:
+        print("Say something!")
+        r.adjust_for_ambient_noise(source, 0.5)
+        audio = r.listen(source, phrase_time_limit=5,
+                         snowboy_configuration=None)
+    # Save audio to file
+    with open(FILENAME, "wb") as f:
+        f.write(audio.get_wav_data())
+
+    audio_file = open(FILENAME, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript["text"]
+
 
 def main():
     API_KEY = "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"
@@ -42,12 +45,12 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen()
-    
+
     print(f"Listening for NAO on {HOST}:{PORT}...")
 
     conn, addr = s.accept()
     message = conn.recv(1024)
-    
+
     print("Received " + message.decode("utf-8"))
     conn.send("Welcome to NAO Chat!".encode("utf-8"))
 
@@ -76,36 +79,36 @@ def main():
     '''
     with conn:
         print(f"Connected to {addr[0]}:{addr[1]}")
-        i=0
-        j=3
+        i = 0
+        j = 3
         while True:
             print("cosa dire?")
             data = stt()
             print(data)
-            i=i+1
+            i = i + 1
             messages.append(
                 {
                     "role": "user",
                     "content": data
                 }
             )
-            
+
             if not data:
                 break
-            
-                
+
             response = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo",
-                messages = messages
+                model="gpt-3.5-turbo",
+                messages=messages
             )
 
             openai_response = response["choices"][0]["message"]["content"]
             print(f"\n{openai_response}\n")
-            if i==j:
+            if i == j:
                 messages.pop[1]
-                i=0
-                
+                i = 0
+
             conn.send(openai_response.encode("utf-8"))
-            
+
+
 if __name__ == "__main__":
     main()
